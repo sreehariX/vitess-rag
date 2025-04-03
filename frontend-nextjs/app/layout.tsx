5fc4c26fdd5d893845f817b3875bd69c3cd4a4a4
sreehariX <sreehariofficial12@gmail.com>
2025-04-03 11:20:05 +0530

    analytics

--- frontend-nextjs/app/layout.tsx
+++ frontend-nextjs/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
